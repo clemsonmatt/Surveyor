d0d606e4952596f313d9122b5348b8d1aae72d5c
Matt Ellis <mellis@ncees.org>
2016-11-29 16:59:02 -0500

    #2 Change manage structure

---
+++ app/controllers/manage/permission_controller.rb
@@ -0,0 +1,4 @@
+class Manage::PermissionController < ApplicationController
+    def new
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
         :survey
 
     namespace :manage do
-        resources :person
+        resources :person,
+            :permission,
+            :default
     end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
